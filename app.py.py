231d49b39f0b6427c88d6bcd9e2490f9b54afc70
Avani Khare <avani.25bai11450@vitbhopal.ac.in>
2026-03-29 19:55:16 +0530

    Initial commit - AI chatbot project

---
+++ app.py.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+# Load data
+data = pd.read_csv("faq.csv")
+
+questions = data['Question']
+answers = data['Answer']
+
+# Vectorize
+vectorizer = TfidfVectorizer()
+X = vectorizer.fit_transform(questions)
+
+def chatbot(user_input):
+    user_vec = vectorizer.transform([user_input])
+    similarity = cosine_similarity(user_vec, X)
+    index = similarity.argmax()
+    return answers[index]
+
+# Run chatbot
+print("College Helpdesk Chatbot (type 'exit' to stop)")
+while True:
+    user = input("You: ")
+    if user.lower() == "exit":
+        break
+    print("Bot:", chatbot(user))
